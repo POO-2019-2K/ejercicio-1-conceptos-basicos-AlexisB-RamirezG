50d2e29d7b74d71cfb774ecbb21ffe92758eb3f9
Your Name <you@example.com>
2019-02-14 14:23:01 -0600

    Ver Final

--- Reservaciones/reservation.js
+++ Reservaciones/reservation.js
@@ -13,18 +13,18 @@ export default class Reservation {
 
     getNights() {
         let aDay = 1000*60*60*24;
-        this._arrDate = this._arrDate.getTime();
+        let arrDateTime = this._arrDate.getTime();
         this._depDate = this._depDate.getTime();
-        let difference = ((this._depDate) - (this._arrDate));
+        let difference = ((this._depDate) - (arrDateTime));
         let nights = (difference)/(aDay);
         console.log(`The number of nights the guest is going to stay is ${nights}`);
     }
 
     getDaysToArrival() {
         let aDay = 1000*60*60*24;
-        this._currentDate = currentDate.getTime();
-        this._arrDate = this._arrDate.getTime();
-        let difference = ((this._depDate) - (this._arrDate));
+        let arrDateTime = this._arrDate.getTime();
+        let currentDateTime = this._currentDate.getTime();
+        let difference = ((arrDateTime) - (currentDateTime));
         let nights = (difference)/(aDay);
         console.log(`The number of nights the guest is going to stay is ${nights}`);
     }
